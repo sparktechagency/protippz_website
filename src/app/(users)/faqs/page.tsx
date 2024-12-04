0080fafb819e6d75bfa580f4ea1d26aef9415f02
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-04 16:36:28 +0600

    s

--- src/app/(users)/faqs/page.tsx
+++ src/app/(users)/faqs/page.tsx
@@ -1,17 +1,33 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Collapse, Typography } from 'antd';
 import Heading from '@/components/Shared/Heading';
+import { get } from '@/ApisRequests/server';
 
 const { Panel } = Collapse;
 const { Title, Text } = Typography;
-
+interface FaqInterface {
+    "_id": string,
+    "question": string,
+    "answer": string,
+    "createdAt": string,
+    "updatedAt": string,
+    "id": string,
+}
 const FAQPage: React.FC = () => {
+    const [data, setData] = useState<FaqInterface[]>()
+    useEffect(() => {
+        const getFaqData = async () => {
+            const res = await get('/manage/get-faq')
+            setData(res?.data || [])
+        }
+        getFaqData()
+    }, [])
     return (
         <div className="container mx-auto px-4 py-8">
-            <Heading 
-            headingText='Frequently Asked Questions'
-            subHeadingText='Common Queries, Clear Solutions'
+            <Heading
+                headingText='Frequently Asked Questions'
+                subHeadingText='Common Queries, Clear Solutions'
             />
             <Collapse
                 accordion
@@ -29,46 +45,19 @@ const FAQPage: React.FC = () => {
                     </span>
                 )}
             >
-                <Panel
-                    header={<span className="text-blue-900 font-semibold">What is PROTIPPZ?</span>}
-                    key="1"
-                    className="custom-panel"
-                >
-                    <Text className="text-green-600">PROTIPPZ is a platform that allows you to interact with your favorite players and teams.</Text>
-                </Panel>
-                <Panel
-                    header={<span className="text-blue-900 font-semibold">How do I sign up for PROTIPPZ?</span>}
-                    key="2"
-                    className="custom-panel"
-                >
-                    <Text className="text-green-600">
-                        Click the “Sign Up” button on the homepage. You can sign up using your Google account or by providing your name, email, username, and password.
-                    </Text>
-                </Panel>
-                <Panel
-                    header={<span className="text-blue-900 font-semibold">How can I tip a player or team?</span>}
-                    key="3"
-                    className="custom-panel"
-                >
-                    <Text className="text-green-600">To tip a player or team, navigate to their profile and use the “Tip” button.</Text>
-                </Panel>
-                <Panel
-                    header={<span className="text-blue-900 font-semibold">What rewards can I earn on PROTIPPZ?</span>}
-                    key="4"
-                    className="custom-panel"
-                >
-                    <Text className="text-green-600">You can earn points, badges, and exclusive rewards based on your interaction and tips.</Text>
-                </Panel>
-                <Panel
-                    header={<span className="text-blue-900 font-semibold">How can I contact support?</span>}
-                    key="5"
-                    className="custom-panel"
-                >
-                    <Text className="text-green-600">You can contact support through the “Help” section on the platform, or by emailing support@protippz.com.</Text>
-                </Panel>
+                {
+                    data?.map((item: FaqInterface) => <Panel
+                        header={<span className="text-blue-900 font-semibold">{item?.question}</span>}
+                        key={item?._id}
+                        className="custom-panel"
+                    >
+                        <Text className="text-green-600">{item?.answer}</Text>
+                    </Panel>)
+                }
             </Collapse>
         </div>
     );
 };
 
 export default FAQPage;
+
