fd37f5e3f4ec24cb6fe2e81760a2a5889ba49887
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-15 10:14:28 +0600

    s

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '192.168.10.11',
+        // hostname: '192.168.10.11',
+        hostname: '3.135.129.190',
         port: '5050', 
       },
     ],
