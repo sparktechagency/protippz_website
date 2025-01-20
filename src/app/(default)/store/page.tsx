435ee3e7fb048bbeb5b6156131815afec3f51c07
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-01-20 04:33:27 -0600

    fix session error

--- src/app/(default)/store/page.tsx
+++ src/app/(default)/store/page.tsx
@@ -1,15 +1,19 @@
-import Heading from '@/components/Shared/Heading'
-import React from 'react'
+import Heading from "@/components/Shared/Heading";
+import Link from "next/link";
+import React from "react";
 
 const StorePage = () => {
-    return (
-        <div>
-            <Heading
-                headingText='STORE'
-                subHeadingText='Coming Soon'
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Heading headingText="STORE" subHeadingText="Coming Soon" />
+      <Link
+        href={`https://www.protippz.store`}
+        className="px-4 py-1 bg-blue-500 rounded-md text-white"
+      >
+        see more
+      </Link>
+    </div>
+  );
+};
 
-export default StorePage
+export default StorePage;
