819a53d73d3b0d95746d7cdfc5ac3fbeb41c2356
hosain ali <hosainahmed534745@gmail.com>
2025-02-27 11:13:08 +0600

    s

--- src/components/HomePage/ShocaseMarquee.tsx
+++ src/components/HomePage/ShocaseMarquee.tsx
@@ -11,27 +11,23 @@ function ShocaseMarquee() {
   }
   return (
     <div className="bg-[#053697]">
-      <Marquee
-        gradient={true}
-        gradientColor="#053697"
-        speed={40}
-      >
+      <Marquee gradient={true} gradientColor="#053697" speed={40}>
         <div className=" flex items-center justify-between py-4 mr-28">
           <div className="text-white font-semibold flex items-center justify-between w-full gap-48">
             <div className="flex gap-2 items-center">
-              <Image src={img} alt="img" width={200} height={200} />
+              <Image className='h-4 w-auto' src={img} alt="img" width={200} height={1200} />
               <h1 className="text-white font-semibold text-nowrap">
                 Choose a Player or Team ⛹️ ⛹️⛹️
               </h1>
             </div>
             <div className="flex gap-2 items-center">
-              <Image src={img} alt="img" width={200} height={200} />
+              <Image className='h-4 w-auto' src={img} alt="img" width={200} height={200} />
               <h1 className="text-white font-semibold text-nowrap">
                 Send them Tippz 💸 💸 💸
               </h1>
             </div>
             <div className="flex gap-2 items-center">
-              <Image src={img} alt="img" width={200} height={200} />
+              <Image className='h-4 w-auto' src={img} alt="img" width={200} height={200} />
               <h1 className="text-white font-semibold text-nowrap mr-48 flex items-center gap-3">
                 Earn Rewards & Win Prizes 🤑🤑🤑
               </h1>
