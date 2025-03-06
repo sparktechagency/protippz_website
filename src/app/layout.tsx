de3693b1ebb1632015d08353ec816ef4b6ae239a
hosain ali <hosainahmed534745@gmail.com>
2025-03-06 08:13:50 +0600

    add google analytics id in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { AuthProvider } from '@/provider/ContextProvider';
 import NextTopLoader from 'nextjs-toploader';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'PROTIPPZ',
   description: 'protippz',
-  keywords:'protippz',
+  keywords: 'protippz',
   icons: {
     icon: '/favicon.png',
   },
@@ -40,6 +41,24 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" type="image/png" />
         <meta name="theme-color" content="#ffffff" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </head>
       <body>
         <NextTopLoader />
