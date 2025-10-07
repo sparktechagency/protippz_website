fca45049d3c06e710aa307ec779c79eeee2127d1
hosainahmeed <skyfal430@gmail.com>
2025-10-07 09:25:40 +0600

    update the announcment modal

--- src/components/popups/AnnouncementPopups.tsx
+++ src/components/popups/AnnouncementPopups.tsx
@@ -3,16 +3,23 @@ import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { X } from 'lucide-react';
 import popup from '@/../public/win-250.jpeg'; // replace with correct path if needed
+import { useRouter } from 'next/navigation';
 
 const COOKIE_NAME = 'announcement_popup_dismissed';
 const SHOW_LATER_COOKIE = 'show_announcement_after';
 
 function AnnouncementPopups() {
     const [isVisible, setIsVisible] = useState(false);
+    const router = useRouter();
+    const [token, setToken] = useState<string | null>(null);
 
     useEffect(() => {
         const isDismissed = Cookies.get(COOKIE_NAME) === 'true';
         const showAfter = Cookies.get(SHOW_LATER_COOKIE);
+        const storedToken = localStorage.getItem("token");
+        if (storedToken) {
+            setToken(storedToken);
+        }
 
         if (showAfter) {
             const showAfterDate = new Date(showAfter);
@@ -31,6 +38,7 @@ function AnnouncementPopups() {
     };
 
     const handleShowLater = () => {
+        router.push('/sign-in');
         const in48Hours = new Date();
         in48Hours.setHours(in48Hours.getHours() + 48);
         Cookies.set(SHOW_LATER_COOKIE, in48Hours.toISOString(), { expires: 2 });
@@ -41,7 +49,7 @@ function AnnouncementPopups() {
 
     if (!isVisible) return null;
 
-    return (
+    return token ? null : (
         <div className='fixed inset-0 bg-black/60 backdrop-blur-sm z-[999] flex items-center justify-center p-4'>
             <div className='bg-white rounded-3xl shadow-2xl max-w-2xl w-full overflow-hidden relative flex flex-col md:flex-row'>
                 {/* Close Button */}
@@ -71,7 +79,7 @@ function AnnouncementPopups() {
                         Win <span className='text-green-500'>$250</span>
                     </h2>
                     <p className='text-gray-600 text-sm md:text-base'>
-                    Don't miss your chance to win $250 – join Protippz today!
+                        Don't miss your chance to win $250 – join PROTIPPZ today!
                     </p>
 
                     <div className='flex flex-col w-full  sm:flex-row gap-3 justify-center md:justify-start mt-4'>
@@ -79,7 +87,7 @@ function AnnouncementPopups() {
                             onClick={handleShowLater}
                             className='w-full sm:w-auto px-5 py-2.5 rounded-full bg-gradient-to-r from-green-500 to-teal-600 text-white font-semibold hover:opacity-90 transition-transform shadow-md'
                         >
-                            Letter
+                            Join Now
                         </button>
                         <button
                             onClick={handleDontShowAgain}
