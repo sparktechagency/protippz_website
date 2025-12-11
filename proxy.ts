eb24b1429e04c71a53619e923286a361f5b2bfd1
hosainahmeed <skyfal430@gmail.com>
2025-12-11 10:07:19 +0600

    patch new update package

---
+++ proxy.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(request: NextRequest) {
+    const token = request.cookies.get("token")?.value || '';
+    const protectedRoutes = ["/rewardz"];
+    if (protectedRoutes.includes(request.nextUrl.pathname)) {
+        if (!token) {
+            return NextResponse.redirect(new URL("/prottiz-special", request.url));
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/rewardz"],
+};
