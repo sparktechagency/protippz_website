f29471360693a20d2572c3eb93f196f70ed16247
manik <maniksarker652@gmail.com>
2025-12-14 12:55:10 +0600

    s

--- src/ApisRequests/Auth.ts
+++ src/ApisRequests/Auth.ts
@@ -1,73 +1,66 @@
-'use server'
+"use server";
 
-import { baseUrl, post } from "./server"
+import { baseUrl } from "./server";
 interface SignUp {
-    "password": string,
-    "confirmPassword": string,
-    "userData": {
-        "name": string,
-        "username": string,
-        "phone": string,
-        "email": string,
-        "address": string,
-
-    }
+  password: string;
+  confirmPassword: string;
+  userData: {
+    name: string;
+    username: string;
+    phone: string;
+    email: string;
+    address: string;
+  };
 }
+//
 export const signUpHandler = async (value: SignUp) => {
-    const url = await baseUrl('user/register-user')
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(value),
-    })
-    return await response.json()
-}
-
-interface verifyCode {
-    "verifyCode": number,
-    "email": string,
-}
-export const OtpVerify = async (value: verifyCode) => {
-    const url = await baseUrl('user/verify-code')
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(value),
-    })
-    return await response.json()
-}
-
-
-interface SignIn {
-    "userNameOrEmail": string,
-    "password": string,
-}
-
-
-
-export const SignInHandler = async (value: SignIn) => {
-    console.log(value)
-    const url = await baseUrl('auth/login')
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(value),
-    })
-    return await response.json()
-}
-
-export interface IverifyEmail {
-    email: string,
-}
-
-
-export const addEmailAddress = async (data: IverifyEmail) => {
-
+  const url = await baseUrl("user/register-user");
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(value),
+  });
+  return await response.json();
 };
 
+interface verifyCode {
+  verifyCode: number;
+  email: string;
+}
+export const OtpVerify = async (value: verifyCode) => {
+  const url = await baseUrl("user/verify-code");
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(value),
+  });
+  return await response.json();
+};
+
+interface SignIn {
+  userNameOrEmail: string;
+  password: string;
+}
+
+export const SignInHandler = async (value: SignIn) => {
+  console.log(value);
+  const url = await baseUrl("auth/login");
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(value),
+  });
+  return await response.json();
+};
+
+export interface IverifyEmail {
+  email: string;
+}
+
+export const addEmailAddress = async (data: IverifyEmail) => {};
